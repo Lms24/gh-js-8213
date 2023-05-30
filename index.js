205cda9605ae57c09a0ea45080b01b810e9aea9a
Lukas Stracke <lukas.stracke@sentry.io>
2023-05-30 10:22:45 +0200

    reproduce problem :(

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 
 Sentry.init({
-  dsn: "https://public@dsn.ingest.sentry.io/1337",
+  dsn: "https://8bffc3913fa54bf0ba5a7645222be038@o447951.ingest.sentry.io/4503970489761792",
   release: "1.0",
   // eslint-disable-next-line deprecation/deprecation
   integrations: [
@@ -17,6 +17,12 @@ Sentry.init({
 
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
+
+const handler = (req, res, next) => {
+  next();
+};
+
+app.use("/route", handler);
 
 app.get("/route", (req, res) => {
   res.send("Hello World!");
